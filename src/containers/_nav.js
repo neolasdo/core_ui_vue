df224c43b75ab8046a26e4ec55ae857db47403e5
Tuan Nguyen <tuannd@gemvietnam.com>
2020-01-09 10:30:57 +0700

    init core ui pr

--- src/containers/_nav.js
+++ src/containers/_nav.js
@@ -7,10 +7,10 @@ export default [
         name: 'Dashboard',
         to: '/dashboard',
         icon: 'cil-speedometer',
-        badge: {
-          color: 'primary',
-          text: 'NEW'
-        }
+        // badge: {
+        //   color: 'primary',
+        //   text: 'NEW'
+        // }
       },
       {
         _name: 'CSidebarNavTitle',
